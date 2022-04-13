1846dd02ee652ed023cc1c82bc791bab4f0da1fa
Fredy,pedro,carol <falexanderbarreto@ucompensar.edu.co>
2022-04-13 12:54:31 -0500

    Se Agrega Proyecto

---
+++ js/login.js
@@ -0,0 +1,18 @@
+function valor(){
+    u = document.getElementById("usuariol").value;
+    p = document.getElementById("pss").value;
+    
+
+    if(u =='118921'){
+        if(p=='123456'){
+            location.href="../GAREP/vista/principal.html";
+        }
+        else{
+            alert("Clave Invalida");
+        }
+    }else
+    {
+        alert("Usuario Invalido");
+    }
+
+}
