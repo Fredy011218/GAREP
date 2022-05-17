9610859637b311f3995d158ab6d06eb118c0f3ea
Fredy011218 <falexanderbarreto@ucompensar.edu.co>
2022-05-17 01:06:04 -0500

    mejora JS

--- js/principal.js
+++ js/principal.js
@@ -78,7 +78,49 @@ function graficaP(){
 function reca(){
   location.href="../vista/prueba.html?";
 }
+function reca2(){
+var combo = document.getElementById("inputcorreo");
+var selected = combo.options[combo.selectedIndex].text;
+  alert("Correo Enviado Correctamente desde: " + selected);
+  location.href="../vista/prueba.html?";
+}
 function aleta(){
   alert("Recuerde seleccionar minimo dos campos de la lista comprobacion");
 }
 
+
+const fechas = new Date();
+var currentTime = new Date();
+
+
+function fecha(){
+  const annActual = fechas.getFullYear();
+  const hoy = fechas.getDate();
+const mesActual = fechas.getMonth() + 1;
+const hora = currentTime.getHours();
+  const min = currentTime.getMinutes();
+  var combo = document.getElementById("Transportadora");
+var selected2 = combo.options[combo.selectedIndex].text;
+  
+  alert("Numero de Guia es: "+"RMA_CO_"+annActual+""+hoy+""+mesActual+""+hora+""+min+"Por transportadora:"+"\n"+selected2);
+}
+
+
+function gestionRMA(){
+  const annActual = fechas.getFullYear();
+  const hoy = fechas.getDate();
+const mesActual = fechas.getMonth() + 1;
+const hora = currentTime.getHours();
+  const min = currentTime.getMinutes();
+  alert("Numero de seguimiento es: "+"RMA_CO_"+annActual+""+hoy+""+mesActual+""+hora+""+min);
+  location.href="../vista/registro.html?";
+}
+function srma(){
+  const annActual2 = fechas.getFullYear();
+  const hoy2 = fechas.getDate();
+const mesActual2 = fechas.getMonth() + 1;
+const hora2 = currentTime.getHours();
+  const min2 = currentTime.getMinutes();
+  var final = "RMA_CO_"+annActual2+""+hoy2+""+mesActual2+""+hora2+""+min2;
+  document.getElementById("rmaid").innerHTML = final.toString();
+}
